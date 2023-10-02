640709b59644d7943037af494064bdb3e5b96483
Alexander <doykov70@gmail.com>
2023-10-02 16:26:20 +0200

    Try to fix working with many messages

--- src/main/java/com/wefox/payment/kafka/KafkaPaymentListener.java
+++ src/main/java/com/wefox/payment/kafka/KafkaPaymentListener.java
@@ -1,19 +1,27 @@
 package com.wefox.payment.kafka;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.wefox.payment.dto.PaymentDTO;
 import com.wefox.payment.enums.PaymentType;
+import com.wefox.payment.models.Account;
 import com.wefox.payment.models.Payment;
 import com.wefox.payment.repositories.AccountRepository;
 import com.wefox.payment.repositories.PaymentRepository;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.stereotype.Service;
-
 import java.sql.Timestamp;
 import java.time.Instant;
 import java.util.Objects;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 
 @Service
@@ -25,19 +33,46 @@ public class KafkaPaymentListener {
     @Autowired
     private AccountRepository accountRepository;
 
+    @Autowired
+    private ObjectMapper mapper;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
     Logger LOG = LoggerFactory.getLogger(KafkaPaymentListener.class);
 
-    @KafkaListener(topics = "offline", groupId = "payment")
-    void listener(PaymentDTO paymentDto) {
+    @KafkaListener(
+            topics = "offline",
+            groupId = "payment"
+    )
+    void offlinePaymentListener(PaymentDTO paymentDto) {
         try {
-            paymentRepository.save(convertToEntity(paymentDto));
-            LOG.info(paymentDto.accountId.toString());
-            LOG.info(paymentDto.amount.toString());
+            savePayment(paymentDto);
+        } catch (IllegalArgumentException e) {
+            LOG.error("Entity that was used was not found " + e.getMessage());
+        }
+    }
+
+    @KafkaListener(
+            topics = "online",
+            groupId = "payment"
+    )
+    void onlinePaymentListener(PaymentDTO paymentDto) {
+        try {
+            if (verifyPayment(paymentDto)) {
+                savePayment(paymentDto);
+            }
         } catch (IllegalArgumentException e) {
             LOG.info("Entity that was used was null or not found " + e.getMessage());
+        } catch (JsonProcessingException e) {
+            LOG.info("PaymentDTO was not serialized successfully " + e.getMessage());
         }
+    }
 
-
+    private void savePayment(PaymentDTO paymentDto) {
+        Payment payment = paymentRepository.save(convertToEntity(paymentDto));
+        Account account = accountRepository.findById(paymentDto.accountId).get();
+        account.setLastPaymentDate(payment.getCreatedAt().toString());
     }
 
     private Payment convertToEntity(PaymentDTO paymentDTO) {
@@ -55,5 +90,18 @@ public class KafkaPaymentListener {
         payment.setCreditCard(paymentDTO.creditCard);
         payment.setAccount(accountRepository.findById(paymentDTO.accountId).get());
         return payment;
+    }
+
+    private boolean verifyPayment(PaymentDTO paymentDto) throws JsonProcessingException {
+        String uri = "http://localhost:9000/payment";
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        HttpEntity<String> entity = new HttpEntity<>(mapper.writeValueAsString(paymentDto), headers);
+        ResponseEntity<?> result =
+                restTemplate.exchange(uri, HttpMethod.POST, entity, Void.class);
+
+        return result.getStatusCode().is2xxSuccessful();
     }
 }
